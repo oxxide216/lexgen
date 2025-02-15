49064ec76f390a9733e957cb86c212ba3c75689f
vie <vie.devr@gmail.com>
2025-02-15 10:19:43 +0300

    Made runtime more correct

--- runtime-src/runtime.c
+++ runtime-src/runtime.c
@@ -23,12 +23,13 @@ static bool row_matches(TransitionRow *row, Str text, u32 *lexeme_len) {
                                   text.ptr[i] > col->max_char))
         continue;
 
+      if (col->min_char == -1)
+        --i;
+
       found = true;
       state = col->next_state;
       if (state == 0) {
-        *lexeme_len = i;
-        if (col->min_char != -1)
-          ++*lexeme_len;
+        *lexeme_len = i + 1;
         return true;
       }
 
@@ -46,27 +47,26 @@ static bool row_matches(TransitionRow *row, Str text, u32 *lexeme_len) {
 
 Str tt_matches(TransitionTable *tt, Str *text, u32 *token_id) {
   Str lexeme = { text->ptr, 0 };
-
-  if (token_id)
-    *token_id = (u32) -1;
+  u32 longest_token_id = (u32) -1;
 
   for (u32 i = 0; i < tt->len; ++i) {
     u32 new_lexeme_len = 0;
     bool row_match = row_matches(tt->items + i, *text,
                                  &new_lexeme_len);
 
-    if (row_match) {
+    if (row_match && new_lexeme_len > lexeme.len) {
       lexeme.len = new_lexeme_len;
-      if (token_id)
-        *token_id = i;
-      break;
+      longest_token_id = i;
     }
   }
 
-  if (*token_id != (u32) -1) {
+  if (longest_token_id != (u32) -1) {
     text->ptr += lexeme.len;
     text->len -= lexeme.len;
   }
+
+  if (token_id)
+    *token_id = longest_token_id;
 
   return lexeme;
 }
