a7bf2bca62b4eabf3f1eb2cf6a5c0195e1475e37
oxxide216 <vie.devr@gmail.com>
2025-09-21 13:49:12 +0300

    Fixed slashes

--- gen-src/main.c
+++ gen-src/main.c
@@ -304,14 +304,12 @@ u32 sb_push_atoms(StringBuilder *sb, Atom *atoms, u32 current_state,
       sb_push_u32(sb, current_state);
       sb_push(sb, ", '");
       if (atom->as._char.is_escaped ||
-          atom->as._char._char == '\'' ||
-          atom->as._char._char == '\\')
+          atom->as._char._char == '\'')
         sb_push_char(sb, '\\');
       sb_push_char(sb, atom->as._char._char);
       sb_push(sb, "', '");
       if (atom->as._char.is_escaped ||
-          atom->as._char._char == '\'' ||
-          atom->as._char._char == '\\')
+          atom->as._char._char == '\'')
         sb_push_char(sb, '\\');
       sb_push_char(sb, atom->as._char._char);
       sb_push(sb, "', ");
